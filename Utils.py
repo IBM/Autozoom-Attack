8782de3a820ef7b2ee0f67279c45334cd7f892d5
Pin-Yu Chen Pin-Yu.Chen@ibm.com <pinyu@dccxl008.pok.ibm.com>
2018-07-04 01:27:01 -0400

    update license disclaimer

--- Utils.py
+++ Utils.py
@@ -1,11 +1,21 @@
 ## Utils.py -- Some utility functions 
 ##
-## Copyright (C) 2018, PaiShun Ting <paishun@umich.edu>
+## Copyright (C) 2018, IBM Corp
 ##                     Chun-Chen Tu <timtu@umich.edu>
+##                     PaiShun Ting <paishun@umich.edu>
 ##                     Pin-Yu Chen <Pin-Yu.Chen@ibm.com>
 ##
-## This program is licenced under the Apache License 2.0,
-## contained in the LICENCE file in this directory.
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
 
 from tensorflow.contrib.keras.api.keras.models import Model, model_from_json, Sequential
 from PIL import Image
